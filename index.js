d78259a22b5a40a6e810d12be3d0426fc24b00ad
Arman Alam <77432880+arman-web-developers@users.noreply.github.com>
2024-06-17 16:00:55 +0530

    Added all files

---
+++ index.js
@@ -0,0 +1,105 @@
+// let number = 4;
+
+// if (number >= 20) {
+//     console.log("positive");
+// } else {
+//     console.log("negative");
+// }
+// =====================================
+// if (number % 2 === 0) {
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+// =====================================
+// let number = 4;
+// let number2 = 12;
+
+// if (number >  number2) {
+//     console.log("number is good");
+// } else {
+//     console.log("number2 is good");
+// }
+// =====================================
+// var numerical = 25;
+
+// let a = 80;
+// let b = 60;
+// let c = 40;
+// let d = 30;
+
+// if (numerical > a) {
+//     console.log("A");
+// } else if (numerical > b) {
+//     console.log("B");
+// } else if (numerical > c) {
+//     console.log("C");
+// } else if (numerical > d) {
+//     console.log("Pass");
+// } else {
+//     console.log("Fail");
+// }
+
+
+// =====================================
+// good way
+// var score = 85;
+// var grade;
+
+// if (score >= 90) {
+//   grade = "A";
+// } else if (score >= 80) {
+//   grade = "B";
+// } else if (score >= 70) {
+//   grade = "C";
+// } else if (score >= 60) {
+//   grade = "D";
+// } else {
+//   grade = "F";
+// }
+// console.log("Grade: " + grade);
+
+// =====================================
+
+// var age = 15;
+// var ticketPrice;
+
+// if (age < 12) {
+//     ticketPrice = 5;
+// } else if (age > 12 && age < 18) {
+//     ticketPrice = 10;
+// } else if (age > 18 && age < 60) {
+//     ticketPrice = 20;
+// } else {
+//     ticketPrice = 15;
+// }
+// console.log("Ticket Price $" + ticketPrice);
+
+// ========================================================
+
+
+// var Amount = 60;
+
+// var discount;
+
+// if (Amount >= 100) {
+//    discount = 20
+// } else if (Amount >= 50) {
+//     discount  = 10
+// } else {
+//     discount  = 0
+// }
+// console.log("Discount : " + discount + "%");
+
+// ==================================================
+
+// var scertnumber = 7;
+// var gusse = 7;
+
+// if (gusse === scertnumber) {
+//     console.log("win");
+// } else  if (gusse < scertnumber) {
+//     console.log("try hight");
+// } else {
+//     console.log("try low");
+// }
